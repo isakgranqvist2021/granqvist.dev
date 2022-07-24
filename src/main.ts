99f5be3cd2de47c99e82889a493aed24b021f047
isakgranqvist2021 <isakwebdev@gmail.com>
2022-07-24 15:09:04 +0200

    Reconfigure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
 import path from 'path';
-import { initHttpsServer } from './https';
 import { initHttpServer } from './http';
 
-const environment = process.env.ENVIRONMENT ?? 'development';
 const app = express();
 
 app.use('/public', express.static(path.resolve('./public')));
@@ -14,8 +12,8 @@ app.get('/', (_, res) => {
   res.sendFile(path.resolve('./public/index.html'));
 });
 
-if (environment === 'production') {
-  initHttpsServer(app);
-}
+// if (environment === 'production') {
+//   initHttpsServer(app);
+// }
 
 initHttpServer(app);
