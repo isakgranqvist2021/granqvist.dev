eca782df59d7dd829cb80ddd6019a6ee7e3ae221
isakgranqvist2021 <isakwebdev@gmail.com>
2022-07-25 19:31:46 +0200

    abc

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,9 +5,9 @@ import path from 'path';
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use('/public', express.static(path.resolve('./public')));
+app.use('/', express.static(path.resolve('./public')));
 
-app.get('/', (_, res) => res.sendFile(path.resolve("./public/index.html")));
+app.get('/', (_, res) => res.sendFile(path.resolve('./public/index.html')));
 
 const port = process.env.ENVIRONMENT === 'production' ? 80 : 8080;
 
