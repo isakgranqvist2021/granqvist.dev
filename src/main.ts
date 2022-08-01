f235fe6c770a9fbbb65b863685fb43c0018efcbb
isakgranqvist2021 <isakwebdev@gmail.com>
2022-08-01 15:39:08 +0200

    Update card design

--- src/main.ts
+++ src/main.ts
@@ -16,5 +16,10 @@ app.use('*', router);
 const port = env.environment === 'production' ? 80 : 8080;
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  if (env.environment !== 'production') {
+    console.log(`Server is listening on http://localhost:${port}`);
+    return;
+  }
+
+  console.log(`Server is listening on https://granqvist.dev`);
 });
