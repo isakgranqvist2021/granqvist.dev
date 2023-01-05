393eee93129050910e46fdca97b05c362fd33b3e
Isak <contact@granqvist.dev>
2023-01-05 18:09:33 +0100

    Add check for env

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,4 +5,11 @@ const env = {
   github_access_token: process.env.GITHUB_ACCESS_TOKEN,
 };
 
+for (const key in env) {
+  if (key === undefined) {
+    console.log(`${key} must be set in order to run`);
+    process.exit(1);
+  }
+}
+
 export default env;
