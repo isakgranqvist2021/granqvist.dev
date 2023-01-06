eccc29c6ffff191daabf8d04bfff46e222822b35
Isak <contact@granqvist.dev>
2023-01-06 02:12:20 +0100

    Use bucket urls

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import env from './config';
 import router from './routers';
 import compression from 'compression';
 import express from 'express';
-import path from 'path';
 
 const app = express();
 
@@ -10,7 +9,6 @@ app.disable('x-powered-by');
 app.set('view engine', '.ejs');
 
 app.use(compression());
-app.use('/public', express.static(path.resolve('./public')));
 app.use('*', router);
 
 const port = process.env.PORT || 8080;
