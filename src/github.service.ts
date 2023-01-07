9645061c958d8d19ae7036af84a858d7a6da9a74
Isak <contact@granqvist.dev>
2023-01-07 03:49:12 +0100

    refactor some stuff

--- src/github.service.ts
+++ src/github.service.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export interface Repository {
+// not a real cache 🤫
+let cache = new Set<Repository>();
+
+interface Repository {
   description: string | null;
   homepage: string | null;
   html_url: string;
@@ -16,44 +19,53 @@ const include = new Set([
   'VPNFinder',
   'DropStore',
   'GodaddyAffiliate',
+  'OpenAI-App',
+  'LitVPNS',
 ]);
 
-const repositoryMapper = (repository: any): Repository | null => {
-  try {
-    if (!include.has(repository.name)) {
+const isNotNull = (repository: object | null): repository is object => {
+  return repository !== null;
+};
+
+export namespace GithubService {
+  const repositoryMapper = (repository: any): Repository | null => {
+    try {
+      if (!include.has(repository.name)) {
+        return null;
+      }
+
+      return {
+        name: repository.name,
+        html_url: repository.html_url,
+        description: repository.description,
+        homepage: repository.homepage,
+        language: repository.language.replaceAll('#', '%23').toLowerCase(),
+        pushed_at: new Date(repository.pushed_at).toLocaleDateString(),
+      };
+    } catch {
       return null;
     }
+  };
 
-    return {
-      name: repository.name,
-      html_url: repository.html_url,
-      description: repository.description,
-      homepage: repository.homepage,
-      language: repository.language.toLowerCase(),
-      pushed_at: new Date(repository.pushed_at).toLocaleDateString(),
-    };
-  } catch {
-    return null;
-  }
-};
+  export const getRepositoriesFromGithub = async (): Promise<
+    Repository[] | null
+  > => {
+    try {
+      if (cache.size !== 0) {
+        return Array.from(cache);
+      }
 
-export const getRepositoriesFromGithub = async (): Promise<
-  Repository[] | null
-> => {
-  try {
-    const res = await axios.get(`https://api.github.com/user/repos`, {
-      headers: {
-        Accept: 'application/vnd.github+json',
-        Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
-      },
-    });
+      const res = await axios.get(`https://api.github.com/user/repos`, {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
+        },
+      });
 
-    const repositories: (Repository | null)[] = res.data.map(repositoryMapper);
-
-    return repositories.filter(
-      (repository): repository is Repository => repository !== null
-    );
-  } catch {
-    return null;
-  }
-};
+      cache = new Set(res.data.map(repositoryMapper).filter(isNotNull));
+      return Array.from(cache);
+    } catch {
+      return null;
+    }
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,24 @@
-import { getRepositoriesFromGithub, Repository } from './github.service';
+import { GithubService } from './github.service';
 import compression from 'compression';
 import 'dotenv/config';
 import express from 'express';
-import type { Request, Response } from 'express';
+
+const requestHandler = async (_: express.Request, res: express.Response) => {
+  try {
+    const repositories = await GithubService.getRepositoriesFromGithub();
+    res.render('index', { repositories });
+  } catch {
+    res.render('index', { repositories: [] });
+  }
+};
 
 const app = express();
 
 app.disable('x-powered-by');
 app.set('view engine', '.ejs');
 app.use(compression());
+app.all('*', requestHandler);
 
-let repositories: Repository[] | null = null;
-app.all('*', async (_: Request, res: Response) => {
-  try {
-    if (!repositories) {
-      repositories = await getRepositoriesFromGithub();
-    }
-
-    res.render('index', { repositories });
-  } catch {
-    res.render('index', { repositories: [] });
-  }
-});
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  if (process.env.ENVIRONMENT !== 'production') {
-    console.log(`Server is listening on http://localhost:${port}`);
-    return;
-  }
-
-  console.log(`Server is listening on https://granqvist.dev`);
-});
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`app listening on ${process.env.PORT || 8080}`)
+);
